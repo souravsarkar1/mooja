8500570e6017a397ca923ea49066ce915bb630b1
souravsarkar1 <souravsantipur9635@gmaill.com>
2026-01-04 17:04:21 +0530

    upate for changes

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://9b8679d4026c.ngrok-free.app/api',
 });
 
 api.interceptors.request.use((config) => {
