8ac062f6b430a65d59516aec74add1161f4b6f11
souravsarkar1 <souravsantipur9635@gmaill.com>
2026-01-08 12:48:41 +0530

    update for minor changes

---
+++ client/src/components/ui/use-mobile.ts
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+const MOBILE_BREAKPOINT = 768
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia(
+      `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+    )
+
+    const handleChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+
+    mediaQuery.addEventListener("change", handleChange)
+    handleChange()
+
+    return () => mediaQuery.removeEventListener("change", handleChange)
+  }, [])
+
+  return !!isMobile
+}
